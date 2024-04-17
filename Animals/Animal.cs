9014f705d5148f87fc31daecea5e386224ad88e8
Koenraad Pecceu <koenraad.pecceu@student.howest.be>
2024-04-17 21:46:24 +0200

    Add project files.

---
+++ Animals/Animal.cs
@@ -0,0 +1,28 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        public int Legs { get; set; }
+        public bool CanFly { get; set; }
+        public int AverageLifeExpectancy { get; set; }
+
+        public Animal(int legs, bool canFly, int averageLifeExpectancy)
+        {
+            Legs = legs;
+            CanFly = canFly;
+            AverageLifeExpectancy = averageLifeExpectancy;
+        }
+
+        /*public Animal(int averageLifeExpectancy)
+        {
+            AverageLifeExpectancy = averageLifeExpectancy;
+        }*/
+
+        public override string ToString()
+        {
+            string fly = "cannot";
+            if (CanFly) fly = "can";
+            return "This animal has " + Legs + " legs and " + fly + " fly.";
+        }
+    }
+}
---
+++ Animals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal(3,true,1000);
+            //Dog dog = new Dog(14, "herdershond");
+            //Bird bird = new Bird(3, new[] {"Rood", "Groen"});
+            Dog dog = new Dog(4, false, 14, "Herdershond");
+            Bird bird = new Bird(2, true, 3, new[] {"Rood", "Groen"});
+
+            Console.WriteLine(animal);
+            Console.WriteLine(dog);
+            Console.WriteLine(dog.Talk());
+            Console.WriteLine(bird);
+            Console.WriteLine(bird.Talk());
+        }
+    }
+}
