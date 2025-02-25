9c4a3a2edbc813292f02313916b98c0976de73ad
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2025-02-25 11:10:00 +0100

    Add animal class

---
+++ Animals/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Animal
+    {
+        public int Legs { get; set; }
+        public bool CanFly { get; set; }
+        public int AverageLifeExpectancy { get; set; }
+
+        public override string ToString()
+        {
+            string canFly = "can";
+
+            // !CanFly is hetzelfde als CanFly == false
+            if (!CanFly)
+            {
+                canFly = "can't";
+            }
+
+            return "This animal has " + Legs + " legs, " 
+                + canFly + " fly and has an average life expectancy of " 
+                + AverageLifeExpectancy + " years.";
+        }
+    }
+}
--- Animals/Program.cs
+++ Animals/Program.cs
@@ -10,7 +10,13 @@ namespace Animals
     {
         static void Main(string[] args)
         {
-            
+            Animal a = new Animal();
+
+            // impliciet wordt er altijd .ToString() toegepast
+            // wanneer je gewoon een 'class' instance in de console plaatst
+            // om hier iets specifieks te zien moet je een override methode
+            // in de class instellen voor ToString()
+            Console.WriteLine(a);
         }
     }
 }
